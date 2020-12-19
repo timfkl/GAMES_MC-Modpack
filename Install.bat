987ad340ae32b32179503b48a81c29b1a6e663d6
ns0906a <ns0906a@student.american.edu>
2020-12-19 18:59:35 -0500

    Edited the Install file. It now works on my computer at the very least.
    As mentioned in the title I tweaked the installation file.
    
    There were several problems I found and fixed.
    
    Without the "" around the directories, any space would not be included. Therefore a path such as C:\Users\Nils Schofer\ will cause the install to break. The "" seemed to have fixed this problem.
    
    The set modpackFolder=... also didn't seem be being called. When echo %modpackFolder% it returned nothing. I moved that definition to the begining and it seemed to have fixed the problem.
    
    Finally, changing every ! to % which fixed some issues as well.
    
    Some things to mention:
    - You have to run Forge at least once for it to add the jar file to the versions folder. Therefore, the code will ask you to reinstall forge every time you run it until you have opened minecraft and run Forge at least once.
    - I can't seem to run the game. I added the profile (copied what you left commented out) to my minecraft launcher. I also realized that Minecraft Launcher already adds %Appdata%\.minecraft when following  the "gameDir". I changed the commented out version of the copy to match this: "gameDir" : "profiles\\"GAMES_Modpack1.0". Unfortunately, as previously stated the game still does not run. It simply gives me a Error Code 0.

--- Install.bat
+++ Install.bat
@@ -1,22 +1,26 @@
 ::@if (@X)==(@Y) @end /* Hybrid line begins JS comment
 @echo off
+echo Loading setup...
 setlocal ENABLEDELAYEDEXPANSION
 title GAMES Modpack Installer
-set installFolder=%APPDATA%\.minecraft
-set profileFile=%APPDATA%\.minecraft\launcher_profiles.json
+set installFolder=%appdata%\.minecraft
+set profileFile=%appdata%\.minecraft\launcher_profiles.json
 set forgeInstall=forge-1.16.4-35.1.4-installer.jar
+set modpackFolder=%installFolder%\profiles\GAMES_Modpack1.0
 
-
-if not exist !installFolder!\versions\1.16.4-forge-35.1.4\1.16.4-forge-35.1.4.jar (
-	start /W !forgeInstall!
+echo Checking for Forge...
+if not exist "%installFolder%\versions\1.16.4-forge-35.1.4\1.16.4-forge-35.1.4.jar" (
+	start /W %forgeInstall%
 	)
+
 ::Makes directories
-if exist %installFolder%\versions\1.16.4-forge-35.1.4\1.16.4-forge-35.1.4.jar (
-	echo Forge-35.1.4.jar exists
-	set modpackFolder=!installFolder!\profiles\GAMES_Modpack1.0
-	mkdir !modpackFolder!\mods
+if exist "%installFolder%\versions\1.16.4-forge-35.1.4\1.16.4-forge-35.1.4.jar" (
 	
-	copy %cd%\mods\*.* !modpackFolder!\mods\
+	echo Forge-35.1.4.jar exists
+	mkdir "%modpackFolder%\mods"
+	
+	echo Installing Mods...
+	copy "%cd%\mods" "%modpackFolder%\mods"
 ) ELSE ("echo ERROR: Forge-35.1.4.jar does not exist")
 
 pause
@@ -25,7 +29,7 @@ pause
 ::*/
 
 ::"822b6abc33b96543a0a69f159dfc80ff" : {
-      ::"gameDir" : "C:\\Users\\Timothy\\AppData\\Roaming\\.minecraft\\profiles\\GAMES_Modpack1.0",
+      ::"gameDir" : "profiles\\GAMES_Modpack1.0",
       ::"icon" : "Cake",
       ::"lastVersionId" : "1.16.4-forge-35.1.4",
       ::"name" : "GAMES_Modpack1.0",
