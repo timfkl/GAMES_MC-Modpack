0218311f9a4a0f9873eca62971fcaef7b90f7e02
timfkl <timfkl11@gmail.com>
2020-12-19 22:36:18 +0100

    Install.bat copy mod files into right place
    Made it so Install.bat copies mod files into a profiles/GAMES_Modpack1.0/mods folder. Still trying to figure out how to automatically make that into the default path for an Installation though.

--- Install.bat
+++ Install.bat
@@ -1,13 +1,33 @@
+::@if (@X)==(@Y) @end /* Hybrid line begins JS comment
 @echo off
+setlocal ENABLEDELAYEDEXPANSION
+title GAMES Modpack Installer
 set installFolder=%APPDATA%\.minecraft
-set profileFile=%APPDATA%\.minecraft/launcher_profiles.json
-set forgeInstall=forge-1.16.4.35.1.4-installer.jar
+set profileFile=%APPDATA%\.minecraft\launcher_profiles.json
+set forgeInstall=forge-1.16.4-35.1.4-installer.jar
 
-::start /W java -jar %forgeInstall%
+
+if not exist !installFolder!\versions\1.16.4-forge-35.1.4\1.16.4-forge-35.1.4.jar (
+	start /W !forgeInstall!
+	)
 ::Makes directories
-if exist %installFolder%\versions\1.16.4-forge.35.1.0 (
-	mkdir %installFolder%\profiles\GAMES_Modpack1.0
-	echo True
-)
-echo False
+if exist %installFolder%\versions\1.16.4-forge-35.1.4\1.16.4-forge-35.1.4.jar (
+	echo Forge-35.1.4.jar exists
+	set modpackFolder=!installFolder!\profiles\GAMES_Modpack1.0
+	mkdir !modpackFolder!\mods
+	
+	copy %cd%\mods\*.* !modpackFolder!\mods\
+) ELSE ("echo ERROR: Forge-35.1.4.jar does not exist")
+
 pause
+::cscript //E.JScript //nologo "%-f0"
+::exit /b 
+::*/
+
+::"822b6abc33b96543a0a69f159dfc80ff" : {
+      ::"gameDir" : "C:\\Users\\Timothy\\AppData\\Roaming\\.minecraft\\profiles\\GAMES_Modpack1.0",
+      ::"icon" : "Cake",
+      ::"lastVersionId" : "1.16.4-forge-35.1.4",
+      ::"name" : "GAMES_Modpack1.0",
+      ::"type" : "custom"
+    ::},
